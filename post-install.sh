95624c5d8e2338bd246c477dacdfc3b30d97663b
Andres Aguilar <aguilarcarboni@gmail.com>
2024-12-13 18:51:25 -0600

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 # Add user to docker group so it can run docker without sudo
+echo "Adding user to docker group\n"
 gpasswd -a aguilarcarboni docker
+echo "Done\n"
+
 
 # Make sure git credentials are stored
+echo "Setting up git credentials\n"
 git config --global credential.helper store
+echo "Done\n"

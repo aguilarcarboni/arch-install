e959ae67423b7c2b086c33e2843fb80349617597
Andres Aguilar <aguilarcarboni@gmail.com>
2024-12-16 23:08:03 -0600

    Update scripts

--- post-install.sh
+++ post-install.sh
@@ -2,67 +2,11 @@
 
 # Make sure git credentials are stored
 echo -e "\nSetting up git credentials..."
+if [ ! -f ~/.git-credentials ]; then
+    echo "Warning: ~/.git-credentials file not found"
+    echo "Please make sure to login to git to create credentials"
+    exit 1
+fi
+
 git config --global credential.helper store
 echo -e "Done\n"
-
-# Create Nebula folder structure
-echo -e "\nCreating Nebula..."
-cd ~/
-mkdir Nebula
-cd Nebula
-
-# Get Nebula's data from Carbonite backups
-
-# Clone Nebula's infrastructure
-echo -e "\nCloning Nebula's infrastructure..."
-git clone https://github.com/aguilarcarboni/nebula.git
-echo -e "Done\n"
-
-# Create Nebula's core
-echo -e "\nCloning Athena from source..."
-mkdir Athena
-cd Athena
-git clone https://github.com/aguilarcarboni/athena-webui.git
-git clone https://github.com/aguilarcarboni/athena-api.git
-git clone https://github.com/aguilarcarboni/athena-speech.git
-echo -e "Building Athena using Nebula's infrastructure..."
-cd ..
-echo -e "\nBuilding Athena using Nebula's infrastructure..."
-cd nebula
-docker compose -f athena.yaml build
-docker compose -f athena.yaml up -d
-cd ..
-echo -e "Done\n"
-
-echo -e "\nCloning laserfocus from source..."
-mkdir Laserfocus
-cd Laserfocus
-git clone https://github.com/aguilarcarboni/oasis.git
-git clone https://github.com/aguilarcarboni/oasis-socket.git
-git clone https://github.com/aguilarcarboni/laserfocus-api.git
-echo -e "Building laserfocus using Nebula's infrastructure..."
-cd ..
-echo -e "\nBuilding Laserfocus using Nebula's infrastructure..."
-cd nebula
-docker compose -f laserfocus.yaml build
-docker compose -f laserfocus.yaml up -d
-cd ..
-echo -e "Done\n"
-
-echo -e "\nBuilding Atlas using Nebula's infrastructure..."
-mkdir Atlas
-cd nebula
-docker compose -f atlas.yaml build
-docker compose -f atlas.yaml up -d
-cd ..
-echo -e "Done\n"
-
-echo -e "\nBuilding Apollo using Nebula's infrastructure..."
-mkdir Apollo
-cd nebula
-docker compose -f apollo.yaml build
-docker compose -f apollo.yaml up -d
-cd ..
-echo -e "Done\n"
-
-echo -e "Done creating Nebula.\n"

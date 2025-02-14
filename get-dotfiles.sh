3174d816a0e00d2780fdb7ae7f806dc6f90d6fea
Andrés Aguilar <73609127+aguilarcarboni@users.noreply.github.com>
2025-02-14 11:48:31 -0600

    Update get-dotfiles.sh

--- get-dotfiles.sh
+++ get-dotfiles.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-# Script to set up main user configuration and dotfiles
-# WARNING: This script is not meant to be run as root. It is meant to be run as the main user.
+###############################################################################
+# Get Dotfiles                                                                #
+###############################################################################
 
-set -e
-set -o pipefail
+echo -e "Getting dotfiles..."
 
 # Clone dotfiles from Github
 echo -e "Cloning dotfiles from Github..."
 git clone https://github.com/aguilarcarboni/dotfiles.git ~/dotfiles
-echo -e "Done\n"
-echo -e "Copying basic dotfiles to home directory..."
 
 # Copy .gnupg to home directory
+echo -e "Copying basic dotfiles to home directory..."
 cp -r ~/dotfiles/.gnupg ~/
 
 # Set permissions for .gnupg
@@ -22,16 +21,17 @@ chmod 700 ~/.gnupg
 
 # Copy .gitconfig to home directory
 cp -r ~/dotfiles/.gitconfig ~/
-echo -e "Done\n"
-echo -e "Decrypting heavier files..."
+
+###############################################################################
+# Decrypt Dotfiles                                                            #
+###############################################################################
 
 # Decrypt .git-credentials
+echo -e "Decrypting heavier files..."
 read -sp "Enter your passphrase to decrypt your files: " passphrase
 echo -e "\n"
 gpg --batch --passphrase ${passphrase} --decrypt ~/dotfiles/.git-credentials.gpg > ~/.git-credentials
-echo -e "Done\n"
 
 # Remove dotfiles folder
 rm -rf ~/dotfiles
-
-echo -e "Dotfiles added successfully"
+echo -e "Done\n"
